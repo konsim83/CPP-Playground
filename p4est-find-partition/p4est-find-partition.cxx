5e0b03693f68a87069078c5e5f2fa56b8336f5e8
Konrad Simon <konrad.simon@uni-hamburg.de>
2020-10-03 00:41:13 +0200

    Changed cmake layout. Added functionality for randimozing uniformly on all processors.

--- p4est-find-partition/p4est-find-partition.cxx
+++ p4est-find-partition/p4est-find-partition.cxx
@@ -11,6 +11,7 @@
 #include <deal.II/distributed/tria.h>
 
 #include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
 #include <deal.II/grid/grid_tools.h>
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/tria_accessor.h>
@@ -38,12 +39,17 @@ class RandomNumberDouble
 {
 public:
   RandomNumberDouble() = delete;
-  RandomNumberDouble(const double _a, const double _b)
+  RandomNumberDouble(const double _a,
+                     const double _b,
+                     const bool   _same_on_all_ranks = true)
     : a(_a)
     , b(_b)
+    , same_on_all_ranks(_same_on_all_ranks)
     , uniform_distribution(a, b)
-    , timeSeed(
-        std::chrono::high_resolution_clock::now().time_since_epoch().count())
+    , timeSeed((
+        same_on_all_ranks ?
+          0.0 :
+          std::chrono::high_resolution_clock::now().time_since_epoch().count()))
     , seed_sequence(std::seed_seq{uint32_t(timeSeed & 0xffffffff),
                                   uint32_t(timeSeed >> 32)})
   {
@@ -55,7 +61,9 @@ public:
   {
     // re-initialize the random number generator with time-dependent seed
     timeSeed =
-      std::chrono::high_resolution_clock::now().time_since_epoch().count();
+      (same_on_all_ranks ?
+         0.0 :
+         std::chrono::high_resolution_clock::now().time_since_epoch().count());
     std::seed_seq seed_sequence{uint32_t(timeSeed & 0xffffffff),
                                 uint32_t(timeSeed >> 32)};
     rng.seed(seed_sequence);
@@ -68,11 +76,17 @@ public:
   }
 
 private:
-  double                                 a, b;
+  double a, b;
+
+  bool same_on_all_ranks;
+
   std::uniform_real_distribution<double> uniform_distribution;
-  uint64_t                               timeSeed;
-  std::seed_seq                          seed_sequence;
-  std::mt19937_64                        rng;
+
+  uint64_t timeSeed;
+
+  std::seed_seq seed_sequence;
+
+  std::mt19937_64 rng;
 };
 
 
@@ -81,9 +95,9 @@ private:
  */
 template <int dim>
 void
-fill_points_randomly(std::vector<Point<dim>> &points,
-                     double                   a = 0,
-                     double                   b = 1)
+fill_points_randomly(std::vector<dealii::Point<dim>> &points,
+                     double                           a = 0,
+                     double                           b = 1)
 {
   RandomNumberDouble random_double_generator(a, b);
 
@@ -120,75 +134,87 @@ public:
   void
   generate_triangualtion(const unsigned int n_refine);
 
+  void
+  write_mesh(const std::string &filename);
+
   unsigned int
-  find_owner_rank(const Point<dim> &p) const;
+  find_owner_rank(const dealii::Point<dim> &p) const;
 
   void
-  find_owner_rank_list(const std::vector<Point<dim>> &points,
-                  std::vector<unsigned int>      mpi_rank_owner) const;
+  find_owner_rank_list(const std::vector<dealii::Point<dim>> &points,
+                       std::vector<unsigned int> mpi_rank_owner) const;
 
 private:
   MPI_Comm mpi_communicator;
 
-  ConditionalOStream pcout;
+  dealii::ConditionalOStream pcout;
 
-  TimerOutput computing_timer;
+  dealii::TimerOutput computing_timer;
 
-  parallel::distributed::Triangulation<dim> triangulation;
+  dealii::parallel::distributed::Triangulation<dim> triangulation;
 
-  typename parallel::distributed::Triangulation<dim>::active_cell_iterator
-    cell_hint;
+  typename dealii::parallel::distributed::Triangulation<
+    dim>::active_cell_iterator cell_hint;
 
   const bool is_periodic;
+
+  bool is_initialized;
 };
 
 
 template <int dim>
 PartitionFinder<dim>::PartitionFinder()
   : mpi_communicator(MPI_COMM_WORLD)
-  , pcout(std::cout, (Utilities::MPI::this_mpi_process(mpi_communicator) == 0))
+  , pcout(std::cout,
+          (dealii::Utilities::MPI::this_mpi_process(mpi_communicator) == 0))
   , computing_timer(mpi_communicator,
                     pcout,
-                    TimerOutput::summary,
-                    TimerOutput::wall_times)
+                    dealii::TimerOutput::summary,
+                    dealii::TimerOutput::wall_times)
   , triangulation(mpi_communicator,
-                  typename Triangulation<dim>::MeshSmoothing(
-                    Triangulation<dim>::smoothing_on_refinement |
-                    Triangulation<dim>::smoothing_on_coarsening))
-  , cell_hint(triangulation.begin_active())
+                  typename dealii::Triangulation<dim>::MeshSmoothing(
+                    dealii::Triangulation<dim>::smoothing_on_refinement |
+                    dealii::Triangulation<dim>::smoothing_on_coarsening))
   , is_periodic(true)
+  , is_initialized(false)
 {}
 
 
 template <int dim>
+void
 PartitionFinder<dim>::generate_triangualtion(const unsigned int n_refine)
 {
-  TimerOutput::Scope t(computing_timer, "mesh generation");
+  dealii::TimerOutput::Scope t(computing_timer, "mesh generation");
 
-  GridGenerator::hyper_cube(triangulation,
-                            0,
-                            1,
-                            /* colorize */ true);
+  dealii::GridGenerator::hyper_cube(triangulation,
+                                    0,
+                                    1,
+                                    /* colorize */ true);
 
   if (is_periodic)
     {
-      std::vector<GridTools::PeriodicFacePair<
-        typename parallel::distributed::Triangulation<dim>::cell_iterator>>
+      std::vector<dealii::GridTools::PeriodicFacePair<
+        typename dealii::parallel::distributed::Triangulation<
+          dim>::cell_iterator>>
         periodicity_vector;
 
       for (unsigned int d = 0; d < dim; ++d)
         {
-          GridTools::collect_periodic_faces(triangulation,
-                                            /*b_id1*/ 2 * (d + 1) - 2,
-                                            /*b_id2*/ 2 * (d + 1) - 1,
-                                            /*direction*/ d,
-                                            periodicity_vector);
+          dealii::GridTools::collect_periodic_faces(triangulation,
+                                                    /*b_id1*/ 2 * (d + 1) - 2,
+                                                    /*b_id2*/ 2 * (d + 1) - 1,
+                                                    /*direction*/ d,
+                                                    periodicity_vector);
         }
 
       triangulation.add_periodicity(periodicity_vector);
     } // if
 
   triangulation.refine_global(n_refine);
+
+  cell_hint = triangulation.begin_active();
+
+  is_initialized = true;
 }
 
 
@@ -196,6 +222,8 @@ template <int dim>
 void
 PartitionFinder<dim>::write_mesh(const std::string &filename)
 {
+  Assert(is_initialized, dealii::ExcNotInitialized());
+
   pcout << "*** Writing mesh ***" << std::endl
         << "*** Dimension:           " << dim << std::endl
         << "*** No. of cells:        " << triangulation.n_active_cells()
@@ -205,10 +233,11 @@ PartitionFinder<dim>::write_mesh(const std::string &filename)
    * Print some general mesh info
    */
   {
-    std::map<types::boundary_id, unsigned int> boundary_count;
+    std::map<dealii::types::boundary_id, unsigned int> boundary_count;
     for (auto &cell : triangulation.active_cell_iterators())
       {
-        for (unsigned int face = 0; face < GeometryInfo<dim>::faces_per_cell;
+        for (unsigned int face = 0;
+             face < dealii::GeometryInfo<dim>::faces_per_cell;
              ++face)
           {
             if (cell->face(face)->at_boundary())
@@ -217,7 +246,7 @@ PartitionFinder<dim>::write_mesh(const std::string &filename)
       }
 
     pcout << "*** Boundary indicators: ";
-    for (const std::pair<const types::boundary_id, unsigned int> &pair :
+    for (const std::pair<const dealii::types::boundary_id, unsigned int> &pair :
          boundary_count)
       {
         pcout << pair.first << "(" << pair.second << " times) ";
@@ -225,68 +254,30 @@ PartitionFinder<dim>::write_mesh(const std::string &filename)
     pcout << std::endl;
   }
 
-  /*
-   * Write a vtu file for each locally owned part of the mesh
-   */
-  DataOut<dim> data_out;
-  data_out.attach_dof_handler(dof_handler);
+  dealii::GridOut grid_out;
 
-  Vector<float> subdomain(triangulation.n_active_cells());
-  for (unsigned int i = 0; i < subdomain.size(); ++i)
-    {
-      subdomain(i) = triangulation.locally_owned_subdomain();
-    }
-  data_out.add_data_vector(subdomain, "subdomain");
-
-  data_out.build_patches();
-
-  std::string filename_slave(filename);
-  filename_slave +=
-    "." + Utilities::int_to_string(triangulation.locally_owned_subdomain(), 4) +
-    ".vtu";
-
-  std::ofstream output(filename_slave.c_str());
-  data_out.write_vtu(output);
-
-
-  /*
-   * Write a pvtu-record to collect all files for each time step.
-   */
-  if (Utilities::MPI::this_mpi_process(mpi_communicator) == 0)
-    {
-      std::vector<std::string> file_list;
-      for (unsigned int i = 0;
-           i < Utilities::MPI::n_mpi_processes(mpi_communicator);
-           ++i)
-        {
-          file_list.push_back(filename + "." + Utilities::int_to_string(i, 4) +
-                              ".vtu");
-        }
-
-      std::string filename_master(filename);
-
-      filename_master += ".pvtu";
-
-      std::ofstream master_output(filename_master);
-      data_out.write_pvtu_record(master_output, file_list);
-    }
-
-  pcout << "*** Written to           " << filename << ".vtu" << std::endl;
+  grid_out.write_mesh_per_processor_as_vtu(triangulation,
+                                           filename,
+                                           /* view_levels */ false,
+                                           /* include_artificials */ false);
 }
 
 
 template <int dim>
 unsigned int
-PartitionFinder<dim>::find_owner_rank(const Point<dim> &p) const
+PartitionFinder<dim>::find_owner_rank(const dealii::Point<dim> &p) const
 {
-  TimerOutput::Scope t(computing_timer,
-                       "finding owner's MPI rank (single point)");
+  Assert(is_initialized, dealii::ExcNotInitialized());
+
+  dealii::TimerOutput::Scope t(computing_timer,
+                               "finding owner's MPI rank (single point)");
+
+  unsigned int mpi_rank = 0;
 
   /*
    * Get access to some p4est internals
    */
   const ForrestType *forrest = triangulation.get_p4est();
-
 
 
   return mpi_rank = 0;
@@ -296,12 +287,15 @@ PartitionFinder<dim>::find_owner_rank(const Point<dim> &p) const
 template <int dim>
 void
 PartitionFinder<dim>::find_owner_rank_list(
-  const std::vector<Point<dim>> &points,
-  std::vector<unsigned int>      mpi_rank_owner) const
+  const std::vector<dealii::Point<dim>> &points,
+  std::vector<unsigned int>              mpi_rank_owner) const
 {
-  TimerOutput::Scope t(computing_timer,
-                       "finding owner's MPI rank (" +
-                         Utilities::int_to_string(points.size()) + " point)");
+  Assert(is_initialized, dealii::ExcNotInitialized());
+
+  dealii::TimerOutput::Scope t(
+    computing_timer,
+    "finding owner's MPI rank (" +
+      dealii::Utilities::int_to_string(points.size()) + " point)");
 }
 
 
@@ -314,17 +308,31 @@ main(int argc, char *argv[])
 {
   try
     {
-      using namespace dealii;
-
       // dimension
-      const int          dim = 2;
-      const unsigned int n_refine = 3;
+      const int          dim      = 2;
+      const unsigned int n_refine = 4;
 
-      Utilities::MPI::MPI_InitFinalize mpi_initialization(
+      dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization(
         argc, argv, dealii::numbers::invalid_unsigned_int);
 
       PartitionFinder<dim> partition_finder;
       partition_finder.generate_triangualtion(n_refine);
+      partition_finder.write_mesh("my_triangulation");
+
+      if (dealii::Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
+        {
+          // generate a number of points
+          unsigned int                    N = 3;
+          std::vector<dealii::Point<dim>> test_points(N, dealii::Point<dim>());
+
+          fill_points_randomly(test_points);
+
+          for (auto &p : test_points)
+            std::cout << "Rank:   "
+                      << dealii::Utilities::MPI::this_mpi_process(
+                           MPI_COMM_WORLD)
+                      << "    Point:   " << p << std::endl;
+        }
     }
   catch (std::exception &exc)
     {
