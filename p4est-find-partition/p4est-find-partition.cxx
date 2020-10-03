1a7ad3b279a7924a7c3668fcd27f65527f7559ab
Konrad Simon <konrad.simon@uni-hamburg.de>
2020-10-03 11:14:00 +0200

    Added dof_handler. Is it really necessary? The triangualtion should be sufficient.

--- p4est-find-partition/p4est-find-partition.cxx
+++ p4est-find-partition/p4est-find-partition.cxx
@@ -137,14 +137,19 @@ public:
   void
   write_mesh(const std::string &filename);
 
-  unsigned int
-  find_owner_rank(const dealii::Point<dim> &p) const;
+  int
+  find_owner_rank(const dealii::Point<dim> &p);
 
   void
   find_owner_rank_list(const std::vector<dealii::Point<dim>> &points,
-                       std::vector<unsigned int> mpi_rank_owner) const;
+                       std::vector<int>                       mpi_rank_owner);
 
 private:
+  boost::optional<dealii::Point<dim>>
+  get_reference_coordinates(
+    const typename dealii::DoFHandler<dim>::active_cell_iterator &cell,
+    const dealii::Point<dim> &                                    point) const;
+
   MPI_Comm mpi_communicator;
 
   dealii::ConditionalOStream pcout;
@@ -153,8 +158,13 @@ private:
 
   dealii::parallel::distributed::Triangulation<dim> triangulation;
 
-  typename dealii::parallel::distributed::Triangulation<
-    dim>::active_cell_iterator cell_hint;
+  dealii::DoFHandler<dim> dof_handler;
+
+  dealii::FE_Q<dim> fe;
+
+  const dealii::Mapping<dim> &mapping;
+
+  typename dealii::DoFHandler<dim>::active_cell_iterator cell_hint;
 
   const bool is_periodic;
 
@@ -175,7 +185,11 @@ PartitionFinder<dim>::PartitionFinder()
                   typename dealii::Triangulation<dim>::MeshSmoothing(
                     dealii::Triangulation<dim>::smoothing_on_refinement |
                     dealii::Triangulation<dim>::smoothing_on_coarsening))
-  , is_periodic(true)
+  , dof_handler(triangulation)
+  , fe(1)
+  , mapping(dealii::StaticMappingQ1<dim>::mapping)
+  , cell_hint(dof_handler.begin_active())
+  , is_periodic(false)
   , is_initialized(false)
 {}
 
@@ -214,6 +228,8 @@ PartitionFinder<dim>::generate_triangualtion(const unsigned int n_refine)
 
   cell_hint = triangulation.begin_active();
 
+  dof_handler.distribute_dofs(fe);
+
   is_initialized = true;
 }
 
@@ -224,7 +240,13 @@ PartitionFinder<dim>::write_mesh(const std::string &filename)
 {
   Assert(is_initialized, dealii::ExcNotInitialized());
 
-  pcout << "*** Writing mesh ***" << std::endl
+  dealii::TimerOutput::Scope t(computing_timer, "write mesh");
+
+  pcout << std::endl
+        << "*** Writing mesh ***" << std::endl
+        << "*** MPI ranks used       "
+        << dealii::Utilities::MPI::n_mpi_processes(mpi_communicator)
+        << std::endl
         << "*** Dimension:           " << dim << std::endl
         << "*** No. of cells:        " << triangulation.n_active_cells()
         << std::endl;
@@ -260,27 +282,88 @@ PartitionFinder<dim>::write_mesh(const std::string &filename)
                                            filename,
                                            /* view_levels */ false,
                                            /* include_artificials */ false);
+
+  pcout << "*** Written to:          " << filename << ".pvtu" << std::endl
+        << std::endl;
 }
 
 
 template <int dim>
-unsigned int
-PartitionFinder<dim>::find_owner_rank(const dealii::Point<dim> &p) const
+int
+PartitionFinder<dim>::find_owner_rank(const dealii::Point<dim> &p)
 {
   Assert(is_initialized, dealii::ExcNotInitialized());
 
-  dealii::TimerOutput::Scope t(computing_timer,
-                               "finding owner's MPI rank (single point)");
-
-  unsigned int mpi_rank = 0;
+  dealii::Timer timer;
+  timer.restart();
 
   /*
    * Get access to some p4est internals
    */
   const ForrestType *forrest = triangulation.get_p4est();
 
+  int mpi_rank = -1;
 
-  return mpi_rank = 0;
+  typename dealii::DoFHandler<dim>::active_cell_iterator cell = cell_hint;
+  if (cell == dof_handler.end())
+    {
+      cell = dof_handler.begin_active();
+    }
+
+  boost::optional<dealii::Point<dim>> qp =
+    get_reference_coordinates(cell_hint, p);
+  if (qp)
+    {
+      /*
+       * If point is found to in the (locally owned) cell = cell_hint
+       * return this_mpi_rank
+       */
+      const dealii::CellId &cell_id = cell->id();
+
+      mpi_rank = dealii::Utilities::MPI::this_mpi_process(mpi_communicator);
+    }
+  else
+    {
+      /*
+       * If point is not found to in the (locally owned) cell = cell_hint
+       * search for it on this processor.
+       */
+      const std::pair<
+        typename dealii::internal::
+          ActiveCellIterator<dim, dim, dealii::DoFHandler<dim>>::type,
+        dealii::Point<dim>>
+        my_pair = dealii::GridTools::find_active_cell_around_point(mapping,
+                                                                   dof_handler,
+                                                                   p);
+
+      cell = my_pair.first;
+      qp   = my_pair.second;
+    }
+
+  if (cell->is_locally_owned())
+    {
+      /*
+       * If the cell found on this processor is neither ghost not artificial
+       * then return this_mpi_rank.
+       */
+      mpi_rank = dealii::Utilities::MPI::this_mpi_process(mpi_communicator);
+    }
+  else
+    {
+      /*
+       * Point is not owned by a local cell.
+       */
+      mpi_rank = -1;
+    }
+
+  timer.stop();
+  std::cout << "---> MPI rank   "
+            << dealii::Utilities::MPI::this_mpi_process(mpi_communicator)
+            << "   search for point   " << p << " ....."
+            << " done in   " << timer.cpu_time() << "   seconds.   "
+            << " Found owner rank   " << mpi_rank << std::endl;
+
+  return mpi_rank;
 }
 
 
@@ -288,7 +371,7 @@ template <int dim>
 void
 PartitionFinder<dim>::find_owner_rank_list(
   const std::vector<dealii::Point<dim>> &points,
-  std::vector<unsigned int>              mpi_rank_owner) const
+  std::vector<int>                       mpi_rank_owner)
 {
   Assert(is_initialized, dealii::ExcNotInitialized());
 
@@ -296,6 +379,30 @@ PartitionFinder<dim>::find_owner_rank_list(
     computing_timer,
     "finding owner's MPI rank (" +
       dealii::Utilities::int_to_string(points.size()) + " point)");
+}
+
+
+template <int dim>
+boost::optional<dealii::Point<dim>>
+PartitionFinder<dim>::get_reference_coordinates(
+  const typename dealii::DoFHandler<dim>::active_cell_iterator &cell,
+  const dealii::Point<dim> &                                    point) const
+{
+  try
+    {
+      dealii::Point<dim> qp = mapping.transform_real_to_unit_cell(cell, point);
+      if (dealii::GeometryInfo<dim>::is_inside_unit_cell(qp))
+        return qp;
+      else
+        return boost::optional<dealii::Point<dim>>();
+    }
+  catch (const typename dealii::Mapping<dim>::ExcTransformationFailed &)
+    {
+      // transformation failed, so
+      // assume the point is
+      // outside
+      return boost::optional<dealii::Point<dim>>();
+    }
 }
 
 
@@ -319,20 +426,28 @@ main(int argc, char *argv[])
       partition_finder.generate_triangualtion(n_refine);
       partition_finder.write_mesh("my_triangulation");
 
-      if (dealii::Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
-        {
-          // generate a number of points
-          unsigned int                    N = 3;
-          std::vector<dealii::Point<dim>> test_points(N, dealii::Point<dim>());
+      /*
+       * Do dome work on process zero only.
+       */
+      //      if (dealii::Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
+      {
+        // generate a number of points
+        unsigned int                    N = 3;
+        std::vector<dealii::Point<dim>> test_points(N, dealii::Point<dim>());
 
-          fill_points_randomly(test_points);
+        fill_points_randomly(test_points);
 
-          for (auto &p : test_points)
+        for (auto &p : test_points)
+          {
             std::cout << "Rank:   "
                       << dealii::Utilities::MPI::this_mpi_process(
                            MPI_COMM_WORLD)
                       << "    Point:   " << p << std::endl;
-        }
+          }
+        std::cout << std::endl;
+
+        partition_finder.find_owner_rank(test_points[0]);
+      }
     }
   catch (std::exception &exc)
     {
